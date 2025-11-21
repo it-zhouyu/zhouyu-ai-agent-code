67aa5c99e7c9b57e0fbedff75c8e52928fe24239
Zhou Yu <497269678@qq.com>
2025-11-21 17:47:51 +0800

    14、SpringAI之MySQL存储ChatMemory

--- spring-ai-demo/src/main/java/com/zhouyu/SpringAIApplication.java
+++ spring-ai-demo/src/main/java/com/zhouyu/SpringAIApplication.java
@@ -4,6 +4,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +23,9 @@ public class SpringAIApplication {
     }
 
     @Bean
-    public ChatMemory chatMemory() {
-        InMemoryChatMemoryRepository inMemoryChatMemoryRepository = new InMemoryChatMemoryRepository();
+    public ChatMemory chatMemory(JdbcChatMemoryRepository chatMemoryRepository) {
         return MessageWindowChatMemory.builder()
-                .chatMemoryRepository(inMemoryChatMemoryRepository)
+                .chatMemoryRepository(chatMemoryRepository)
                 .build();
     }
 
