693de747635918fe8656c9b68565b4e8a73df039
Zhou Yu <497269678@qq.com>
2025-12-05 17:19:21 +0800

    45、AI-Data之报告生成和人类反馈

--- ai-data/src/main/java/com/zhouyu/DataController.java
+++ ai-data/src/main/java/com/zhouyu/DataController.java
@@ -1,10 +1,19 @@
 package com.zhouyu;
 
 import com.alibaba.cloud.ai.graph.CompiledGraph;
+import com.alibaba.cloud.ai.graph.NodeOutput;
 import com.alibaba.cloud.ai.graph.OverAllState;
+import com.alibaba.cloud.ai.graph.RunnableConfig;
+import com.alibaba.cloud.ai.graph.action.InterruptionMetadata;
+import com.alibaba.cloud.ai.graph.streaming.StreamingOutput;
+import com.alibaba.fastjson2.JSON;
+import com.zhouyu.dto.Plan;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.Map;
 
@@ -14,15 +23,66 @@ import java.util.Map;
  * 微信号：it_zhouyu
  */
 @RestController
+@Log4j2
 public class DataController {
 
     @Autowired
     private CompiledGraph compiledGraph;
 
-    @GetMapping("/stream")
-    public Map<String, Object> stream(String input) {
+    @GetMapping(value = "/stream", produces = "text/html;charset=UTF-8")
+    public Flux<String> stream(String input) {
 
-        OverAllState state = compiledGraph.invoke(Map.of("input", input)).orElseThrow();
-        return state.data();
+        RunnableConfig config = RunnableConfig.builder().threadId("123").build();
+
+        Flux<NodeOutput> outputFlux = compiledGraph.stream(Map.of("input", input), config);
+
+        return outputFlux.map(output -> {
+            if (output instanceof StreamingOutput<?> streamingOutput) {
+                log.info("output: {}", output);
+                if (streamingOutput.message() != null && streamingOutput.node().equals("reportGeneratorNode")) {
+                    return streamingOutput.message().getText();
+                }
+            } else if (output instanceof InterruptionMetadata interruptionMetadata) {
+                Plan plan = output.state().value("plannerResult", Plan.class).orElseThrow();
+                return "计划生成完成，请确认! " + JSON.toJSONString(plan);
+            }
+            return "";
+        });
+    }
+
+    @Autowired
+    private ChatClient.Builder chatClientBuilder;
+
+    @GetMapping(value = "/streamContinue", produces = "text/html;charset=UTF-8")
+    public Flux<String> streamContinue(String input) throws Exception {
+        RunnableConfig config = RunnableConfig.builder().threadId("123").build();
+
+        Plan plan = compiledGraph.getState(config).state().value("plannerResult", Plan.class).orElseThrow();
+
+        ChatClient chatClient = chatClientBuilder.build();
+        Plan newPlan = chatClient
+                .prompt()
+                .system("按用户要求修改计划，以下是当前计划：" + JSON.toJSONString(plan))
+                .user("用户要求：" + input)
+                .call()
+                .entity(Plan.class);
+
+        RunnableConfig updatedConfig = compiledGraph.updateState(config, Map.of("plannerResult", newPlan));
+
+        RunnableConfig resumeConfig = RunnableConfig.builder(updatedConfig)
+                .addMetadata(RunnableConfig.HUMAN_FEEDBACK_METADATA_KEY, "placeholder")
+                .build();
+
+        Flux<NodeOutput> outputFlux = compiledGraph.streamFromInitialNode(compiledGraph.getState(updatedConfig).state(), resumeConfig);
+
+        return outputFlux.map(output -> {
+            if (output instanceof StreamingOutput<?> streamingOutput) {
+                log.info("output: {}", output);
+                if (streamingOutput.message() != null && streamingOutput.node().equals("reportGeneratorNode")) {
+                    return streamingOutput.message().getText();
+                }
+            }
+            return "";
+        });
     }
 }
--- ai-data/src/main/java/com/zhouyu/node/ReportGeneratorNode.java
+++ ai-data/src/main/java/com/zhouyu/node/ReportGeneratorNode.java
@@ -2,7 +2,15 @@ package com.zhouyu.node;
 
 import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.action.NodeAction;
+import com.alibaba.fastjson2.JSON;
+import com.zhouyu.dto.Plan;
+import com.zhouyu.dto.StepResultDto;
+import com.zhouyu.util.PromptUtil;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import reactor.core.publisher.Flux;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -11,8 +19,28 @@ import java.util.Map;
  * 微信号：it_zhouyu
  */
 public class ReportGeneratorNode implements NodeAction {
+    private ChatClient chatClient;
+
+    public ReportGeneratorNode(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
     @Override
     public Map<String, Object> apply(OverAllState state) throws Exception {
-        return Map.of();
+        HashMap<String, StepResultDto> planExecuteResult = state.value("planExecuteResult", new HashMap<String, StepResultDto>());
+        String userInput = state.value("input", String.class).orElseThrow();
+        Plan plan = state.value("plannerResult", Plan.class).orElseThrow();
+
+        String prompt = PromptUtil.getPrompt("report");
+        prompt = prompt.replace("{{user_input}}", userInput);
+        prompt = prompt.replace("{{plan}}", JSON.toJSONString(plan));
+        prompt = prompt.replace("{{stepResult}}", JSON.toJSONString(planExecuteResult));
+
+        Flux<ChatResponse> content = chatClient.prompt()
+                .user(prompt)
+                .stream()
+                .chatResponse();
+
+        return Map.of("reportGeneratorResult", content);
     }
 }
