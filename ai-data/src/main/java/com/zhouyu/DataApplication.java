05ee956aa63eb62b7ea5a9d3856cf6dd8596b07f
Zhou Yu <497269678@qq.com>
2025-12-05 16:58:33 +0800

    44、AI-Data之计划生成、执行、失败重试

--- ai-data/src/main/java/com/zhouyu/DataApplication.java
+++ ai-data/src/main/java/com/zhouyu/DataApplication.java
@@ -1,16 +1,21 @@
 package com.zhouyu;
 
 import com.alibaba.cloud.ai.graph.CompiledGraph;
+import com.alibaba.cloud.ai.graph.GraphRepresentation;
+import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.StateGraph;
+import com.alibaba.cloud.ai.graph.action.AsyncEdgeAction;
 import com.alibaba.cloud.ai.graph.action.AsyncNodeAction;
+import com.alibaba.cloud.ai.graph.action.EdgeAction;
 import com.alibaba.cloud.ai.graph.exception.GraphStateException;
-import com.zhouyu.node.KeywordsExtractNode;
-import com.zhouyu.node.TableInfoRecallNode;
+import com.zhouyu.node.*;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 import static com.alibaba.cloud.ai.graph.action.AsyncNodeAction.node_async;
 
@@ -27,12 +32,30 @@ public class DataApplication {
         StateGraph stateGraph = new StateGraph();
         stateGraph.addNode("keywordsExtractNode", node_async(new KeywordsExtractNode(chatClientBuilder.build())));
         stateGraph.addNode("tableInfoRecallNode", node_async(new TableInfoRecallNode(vectorStore)));
+        stateGraph.addNode("plannerNode", node_async(new PlannerNode(chatClientBuilder.build())));
+        stateGraph.addNode("planExecuteNode", node_async(new PlanExecuteNode(chatClientBuilder.build())));
+        stateGraph.addNode("sqlExecuteNode", node_async(new SqlExecuteNode()));
+        stateGraph.addNode("reportGeneratorNode", node_async(new ReportGeneratorNode()));
+
 
         stateGraph.addEdge(StateGraph.START, "keywordsExtractNode")
                 .addEdge("keywordsExtractNode", "tableInfoRecallNode")
-                .addEdge("tableInfoRecallNode", StateGraph.END);
+                .addEdge("tableInfoRecallNode", "plannerNode")
+                .addEdge("plannerNode", "planExecuteNode")
+                .addConditionalEdges("planExecuteNode", AsyncEdgeAction.edge_async(new EdgeAction() {
+                    @Override
+                    public String apply(OverAllState state) throws Exception {
+                        return state.value("planExecuteNextNode", String.class).orElseThrow();
+                    }
+                }), Map.of("sql", "sqlExecuteNode", "report", "reportGeneratorNode"))
+                .addEdge("sqlExecuteNode", "planExecuteNode")
+                .addEdge("reportGeneratorNode", StateGraph.END);
 
-        return stateGraph.compile();
+        CompiledGraph compiledGraph = stateGraph.compile();
+        GraphRepresentation representation = compiledGraph.getGraph(GraphRepresentation.Type.MERMAID);
+        System.out.println(representation.content());
+
+        return compiledGraph;
     }
 
     public static void main(String[] args) {
