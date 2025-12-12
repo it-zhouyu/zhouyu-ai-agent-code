dc167ec31bc30014f2910ec6055d0b8ec85ca9bc
Zhou Yu <497269678@qq.com>
2025-12-12 08:55:37 +0800

    73、AI程序员之前端工程师Agent开发

--- ai-engineer/src/main/java/com/zhouyu/EngineerApplication.java
+++ ai-engineer/src/main/java/com/zhouyu/EngineerApplication.java
@@ -5,6 +5,7 @@ import com.alibaba.cloud.ai.graph.agent.ReactAgent;
 import com.alibaba.cloud.ai.graph.agent.extension.interceptor.FilesystemInterceptor;
 import com.alibaba.cloud.ai.graph.agent.extension.tools.filesystem.ReadFileTool;
 import com.alibaba.cloud.ai.graph.agent.flow.agent.SequentialAgent;
+import com.alibaba.cloud.ai.graph.agent.interceptor.contextediting.ContextEditingInterceptor;
 import com.alibaba.cloud.ai.graph.exception.GraphRunnerException;
 import com.alibaba.cloud.ai.graph.exception.GraphStateException;
 import com.alibaba.cloud.ai.graph.streaming.StreamingOutput;
@@ -40,13 +41,16 @@ public class EngineerApplication {
                         # 职责
                         - 根据用户需求进行架构设计
                         - 主要做技术选型、接口设计、表设计
-                        - 如果涉及到前端，就采用前后端分离
+                        - 如果涉及到前端，就采用前后端分离的架构
+                        - 代码工程目录也要区分前端和后端
                         
                         # 注意
                         - 你只需要生成架构设计方案，不需要实现具体的前端和后端代码
                         - 项目工作目录为：{workspace}
                         - 你只需要将架构设计方案保存到本地文件系统，不需要实现前端和后端代码，更不需要去保存代码
                         - 如果项目工作目录下已经有了架构设计方案，就不需要再生成了
+                        - 逐个调用工具，不要一次性调用多个工具
+                        
                         """.replace("{workspace}", "./zhouyu-code"))
                 .build();
     }
@@ -67,16 +71,45 @@ public class EngineerApplication {
                         
                         # 注意
                         - 项目工作目录为：{workspace}
+                        - 逐个调用工具，不要一次性调用多个工具
                         - 你只需要写后端代码，不需要写前端代码
                         """.replace("{workspace}", "./zhouyu-code"))
                 .build();
     }
 
     @Bean
-    public SequentialAgent sequentialAgent(ReactAgent architectAgent, ReactAgent backendAgent) throws GraphStateException {
+    public ReactAgent frontendAgent(ChatModel chatModel) {
+        ContextEditingInterceptor contextEditingInterceptor = ContextEditingInterceptor.builder()
+                .trigger(1000)
+                .clearToolInputs(true)
+                .build();
+
+        return ReactAgent.builder()
+                .name("frontendAgent")
+                .model(chatModel)
+                .tools(ToolCallbacks.from(new FileTool()))
+                .interceptors(contextEditingInterceptor)
+                .systemPrompt("""
+                        你是一名资深前端开发工程师，负责实现前端代码
+                        
+                        # 职责
+                        - 根据用户需求和架构方案开发前端代码，不需要读取后端代码
+                        - 你必须从项目工作目录中读取架构设计方案来实现前端代码
+                        - 将你写的前端代码保存到本地项目工作目录中
+                        
+                        # 注意
+                        - 项目工作目录为：{workspace}
+                        - 逐个调用工具，不要一次性调用多个工具
+                        - 你只需要写前端代码，不需要写后端代码
+                        """.replace("{workspace}", "./zhouyu-code"))
+                .build();
+    }
+
+    @Bean
+    public SequentialAgent sequentialAgent(ReactAgent architectAgent, ReactAgent backendAgent, ReactAgent frontendAgent) throws GraphStateException {
         return SequentialAgent.builder()
                 .name("sequentialAgent")
-                .subAgents(List.of(architectAgent, backendAgent))
+                .subAgents(List.of(architectAgent, backendAgent, frontendAgent))
                 .build();
     }
 
