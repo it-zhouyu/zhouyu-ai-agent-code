f8d833b635f09460c42e45890d9c92653375336c
Zhou Yu <497269678@qq.com>
2025-12-13 09:10:45 +0800

    78、多模态之DashScope视频总结、音频提取、字幕总结

--- ai-multi-model/src/main/java/com/zhouyu/DashscopeService.java
+++ ai-multi-model/src/main/java/com/zhouyu/DashscopeService.java
@@ -8,6 +8,7 @@ import com.alibaba.dashscope.common.Role;
 import com.alibaba.dashscope.exception.NoApiKeyException;
 import com.alibaba.dashscope.exception.UploadFileException;
 import com.alibaba.dashscope.utils.JsonUtils;
+import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -27,6 +28,7 @@ public class DashscopeService {
 
     @Value("${spring.ai.dashscope.api-key}")
     private String apiKey;
+
     public String imageGenerate(String prompt, String model, Map<String, Object> parameters) throws NoApiKeyException, UploadFileException {
 
         MultiModalConversation conv = new MultiModalConversation();
@@ -54,7 +56,7 @@ public class DashscopeService {
         MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
                 .content(List.of(
 //                        Map.of("image", imageUrl != null ? imageUrl : "file://"+localPath),
-                        Map.of("image", imageUrl != null ? imageUrl : "data:image/png;base64,"+encodeImageToBase64),
+                        Map.of("image", imageUrl != null ? imageUrl : "data:image/png;base64," + encodeImageToBase64),
                         Map.of("text", prompt))).build();
         MultiModalConversationParam param = MultiModalConversationParam.builder()
                 .apiKey(apiKey)
@@ -66,10 +68,69 @@ public class DashscopeService {
         return JsonUtils.toJson(result);
     }
 
+    public Flowable<MultiModalConversationResult> videoChat(String videoUrl, String localPath, String prompt, String model) throws NoApiKeyException, UploadFileException, IOException {
+        MultiModalConversation conv = new MultiModalConversation();
+        MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
+                .content(List.of(
+                        Map.of("video", videoUrl != null ? videoUrl : "file://" + localPath, "fps", 2),
+//                        Map.of("video", videoUrl != null ? videoUrl : "data:video/mp4;base64," + encodeVideoToBase64(localPath)),
+                        Map.of("text", prompt))).build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                .apiKey(apiKey)
+                .model(model)
+                .messages(List.of(userMessage))
+                .build();
+
+//        MultiModalConversationResult result = conv.call(param);
+//        return JsonUtils.toJson(result);
+        return conv.streamCall(param);
+    }
+
+    public String audioChat(String videoUrl, String localPath, String model) throws NoApiKeyException, UploadFileException, IOException {
+        MultiModalConversation conv = new MultiModalConversation();
+        MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
+                .content(List.of(Map.of("audio", videoUrl != null ? videoUrl : "file://" + localPath))).build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                .apiKey(apiKey)
+                .model(model)
+                .messages(List.of(userMessage))
+                .build();
+
+        MultiModalConversationResult result = conv.call(param);
+        return result.getOutput().getChoices().get(0).getMessage().getContent().get(0).get("text").toString();
+//        return conv.streamCall(param);
+    }
+
     private String encodeImageToBase64(String imagePath) throws IOException {
         Path path = Paths.get(imagePath);
         byte[] imageBytes = Files.readAllBytes(path);
         return Base64.getEncoder().encodeToString(imageBytes);
+    }
+
+    private static String encodeVideoToBase64(String videoPath) throws IOException {
+        Path path = Paths.get(videoPath);
+        byte[] videoBytes = Files.readAllBytes(path);
+        return Base64.getEncoder().encodeToString(videoBytes);
+    }
+
+    public Path extractAudio(Path videoPath) {
+        try {
+            String videoFilePath = videoPath.toString();
+            String audioFilePath = videoFilePath.substring(0, videoFilePath.lastIndexOf('.')) + ".mp3";
+
+            ProcessBuilder processBuilder = new ProcessBuilder("ffmpeg", "-i", videoFilePath, "-vn", audioFilePath);
+            Process process = processBuilder.start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("FFmpeg execution failed with exit code: " + exitCode);
+            }
+
+            return Path.of(audioFilePath);
+        } catch (Exception e) {
+            throw new RuntimeException("Error extracting audio from video", e);
+        }
+
     }
 
 }
--- ai-multi-model/src/main/java/com/zhouyu/MultiModelController.java
+++ ai-multi-model/src/main/java/com/zhouyu/MultiModelController.java
@@ -2,17 +2,24 @@ package com.zhouyu;
 
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.image.DashScopeImageModel;
+import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversationResult;
 import com.alibaba.dashscope.exception.NoApiKeyException;
 import com.alibaba.dashscope.exception.UploadFileException;
+import io.reactivex.Flowable;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.content.Media;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -38,6 +45,9 @@ public class MultiModelController {
 
     @Autowired
     private DashscopeService dashscopeService;
+
+    @Autowired
+    private ChatClient.Builder chatClientBuilder;
 
     @GetMapping("/imageGeneration")
     public String imageGeneration() {
@@ -109,5 +119,63 @@ public class MultiModelController {
             throw new RuntimeException(e);
         }
         return result;
+    }
+
+    // 异步调用
+    // https://help.aliyun.com/zh/model-studio/text-to-video-api-reference?spm=a2c4g.11186623.help-menu-2400256.d_2_3_3.4b0a11fc5PS841&scm=20140722.H_2865250._.OR_help-T_cn~zh-V_1#ecd1180f3c026
+
+    @GetMapping(path = "/videoChat", produces = "text/html;charset=UTF-8")
+    public Flowable<String> videoChat() {
+        try {
+            Flowable<MultiModalConversationResult> result = dashscopeService.videoChat(null,"/Users/dadudu/idea/java/vip/zhouyu-ai-agent-vip/ai-multi-model/src/main/resources/ToolCallLimitHook.mp4", "总结一下视频", "qwen3-vl-plus");
+//            Flowable<MultiModalConversationResult> result = dashscopeService.videoChat("https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241115/cqqkru/1.mp4",null, "总结一下视频", "qwen3-vl-plus");
+
+            Flowable<String> stringFlowable = result.map(multiModalConversationResult ->
+                    multiModalConversationResult.getOutput().getChoices().get(0).getMessage().getContent().get(0).get("text").toString()
+            );
+            return stringFlowable;
+        } catch (NoApiKeyException e) {
+            throw new RuntimeException(e);
+        } catch (UploadFileException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @GetMapping(path = "/audioChat", produces = "text/html;charset=UTF-8")
+    public String audioChat() {
+        try {
+
+            String filePath = "/Users/dadudu/idea/java/vip/zhouyu-ai-agent-vip/ai-multi-model/src/main/resources/ToolCallLimitHook.mp4";
+            Path audioPath = dashscopeService.extractAudio(Path.of(filePath));
+            String result = dashscopeService.audioChat(null,audioPath.toAbsolutePath().toString(), "qwen3-asr-flash");
+
+            String promptString = """
+                你是一个专业的视频内容分析师。
+                请根据以下由视频转录的文本，提供一份简洁、精确、易于阅读的内容摘要。
+                你的摘要应该包含视频的核心观点和关键信息。
+
+                请使用中文进行总结。
+
+                转录文本如下:
+                ---
+                {transcript}
+                ---
+                """;
+
+            PromptTemplate promptTemplate = new PromptTemplate(promptString);
+            String prompt = promptTemplate.render(Map.of("transcript", result));
+
+            ChatClient chatClient = chatClientBuilder.build();
+            return chatClient.prompt(prompt).call().content();
+        } catch (NoApiKeyException e) {
+            throw new RuntimeException(e);
+        } catch (UploadFileException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
